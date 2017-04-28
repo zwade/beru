b7e4d30e8c86813eddd31410fea5588f8d62d4f7
Zachary Wade <zacharywade@gmail.com>
2017-04-28 15:53:00 -0400

    pushing

--- sample/__init__.py
+++ sample/__init__.py
@@ -14,12 +14,12 @@ def sliding_window(array, size, fn):
 	return [fn(array[max(0, i-size):min(len(array)-1, i+size)]) for i in range(len(array))]
 
 def sliding_average(array, size):
-	base = sum(array[:size*2+1])/(size*2+1)
+	base = sum(array[:size*2+1])//(size*2+1)
 	out = [array[i] for i in range(len(array))]
 	for i in range(size, len(array)-size):
 		out[i] = base
-		base -= array[i-size]/(size*2+1)
-		base += array[i+size]/(size*2+1)
+		base -= array[i-size]//(size*2+1)
+		base += array[i+size]//(size*2+1)
 	return out
 
 def unzip(a):
@@ -29,7 +29,7 @@ def inline_avg(array, idx, a_length = 70):
 	lower = max(0, idx-a_length)
 	upper = min(len(array), idx+a_length)
 
-	return sum(array[lower:upper])/(upper-lower)
+	return sum(array[lower:upper])//(upper-lower)
 
 
 class Sample:
@@ -77,11 +77,11 @@ class Sample:
 			return self.fqs, self.dft
 
 		data = np.abs(self.data)
-		dft = np.fft.fft(data)[:len(data)/2]
+		dft = np.fft.fft(data)[:len(data)//2]
 		dft = np.abs(dft)
 		dft = np.vectorize(math.log)(dft)
 		dft = sliding_window(dft, 5, max)
-		fqs = fftpack.fftfreq(len(data),float(1)/self.RATE)[:len(data)/2]
+		fqs = fftpack.fftfreq(len(data),float(1)/self.RATE)[:len(data)//2]
 		
 		self.fqs = fqs
 		self.dft = dft
@@ -97,14 +97,13 @@ class Sample:
 	def get_data(self, points = 1024):
 		fqs, dft = self.get_frequency_data()
 		cutoff = np.searchsorted(fqs, BASE_CUTOFF)
-		highest = max(dft[cutoff:])
 		
 		if type(points) == int:
-			INTVL = min(len(dft)-cutoff, 20000)/points
+			INTVL = min(len(dft)-cutoff, 20000)//points
 			points = [BASE_CUTOFF + INTVL * i for i in range(points)]
 
 		idxs = np.searchsorted(fqs, points)
-		results = [(inline_avg(dft, i)/highest, j) 
+		results = [(inline_avg(dft, i), j) 
 				if i < len(dft) else (0,j) for (i,j) in zip(idxs, points)]
 		results = filter(lambda x: x is not None, results)
 
@@ -116,7 +115,7 @@ class Sample:
 		size = float(size)
 
 		sample_len  = self.RATE*size
-		num_samples = int(math.ceil(self.FRAMES/sample_len))
+		num_samples = int(math.ceil(self.FRAMES//sample_len))
 		data = [None for i in range(num_samples)]
 
 		for i in range(num_samples):
@@ -135,6 +134,8 @@ def get_all_in_path(p, points = 1024, bucket_len = 0.5):
 			frqs, amps = s.get_data(points)
 			current = current + amps
 
+		highest = max(amps)
+		current = [i / float(highest) for i in current]
 		if sample_name not in samples:
 			samples[sample_name] = (frqs, [])
 		samples[sample_name][1].append(current)
